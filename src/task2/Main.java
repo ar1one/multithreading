f5a30692cf0d4e70202c223ee82ae2033a291748
Artur Belyaev <belyaev.96@mail.ru>
2023-04-02 17:21:16 +0300

    Initial commit

---
+++ src/task2/Main.java
@@ -0,0 +1,69 @@
+package task2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        String[] texts = new String[25];
+        for (int i = 0; i < texts.length; i++) {
+            texts[i] = generateText("aab", 30_000);
+        }
+
+        long startTs = System.currentTimeMillis(); // start time
+        ExecutorService threadPool = Executors.newFixedThreadPool(texts.length);
+        List<Future<Integer>> threads = new ArrayList<>();
+
+        for (String text : texts) {
+
+            Future<Integer> task = threadPool.submit(() -> {
+                int maxSize = 0;
+                for (int i = 0; i < text.length(); i++) {
+                    for (int j = 0; j < text.length(); j++) {
+                        if (i >= j) {
+                            continue;
+                        }
+                        boolean bFound = false;
+                        for (int k = i; k < j; k++) {
+                            if (text.charAt(k) == 'b') {
+                                bFound = true;
+                                break;
+                            }
+                        }
+                        if (!bFound && maxSize < j - i) {
+                            maxSize = j - i;
+                        }
+                    }
+                }
+                System.out.println(text.substring(0, 100) + " -> " + maxSize);
+                return maxSize;
+            });
+            threads.add(task);
+        }
+        int maxValue = 0;
+        for (Future<Integer> task : threads) {
+            if (task.get() > maxValue) maxValue = task.get();
+        }
+        threadPool.shutdown();
+
+        long endTs = System.currentTimeMillis(); // end time
+
+        System.out.println("Time: " + (endTs - startTs) + "ms");
+        System.out.println("Максимальный интервал значений среди всех строк: " + maxValue);
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+}
+
